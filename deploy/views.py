f8119a20a41af125fecb1e0e0b71e6ec4ed9cdda
vansh saluja <vanshsaluja200@gmail.com>
2024-05-18 21:01:02 -0300

    initial

---
+++ deploy/views.py
@@ -0,0 +1,15 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class EmployeeView(APIView):
+    # myEmp = [{
+    #     "name": "vansh",
+    #     "occupation": "Software dev"
+    # }, {
+    #     "name": "rohan",
+    #     "occupation": "UI/UX Desginer"
+    # }]
+
+    def get(self, request):
+        return Response({"message": "Hello world"})
